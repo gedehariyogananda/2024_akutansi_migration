a1ab53e17cdf91dcfd35e70a6162cf6d4c908194
gedehariyogananda <gedehariyoga@gmail.com>
2024-09-20 10:13:56 +0700

    feat: code company unique setup

--- db/migrations/20240826081138_create_companies_table.sql
+++ db/migrations/20240826081138_create_companies_table.sql
@@ -3,6 +3,7 @@
 SET TIME ZONE 'Asia/Jakarta';
 CREATE TABLE companies (
     id VARCHAR(100) PRIMARY KEY DEFAULT uuid_generate_v4(),
+    code_company VARCHAR(50) NOT NULL UNIQUE,
     name VARCHAR(255) NOT NULL,
     address TEXT NOT NULL,
     image_company VARCHAR(255) NOT NULL,
@@ -10,8 +11,8 @@ CREATE TABLE companies (
     updated_at TIMESTAMP WITH TIME ZONE
 );
 
-INSERT INTO companies (id,name, address, image_company, created_at, updated_at) VALUES
-('073cebf0-4c4a-5d4f-9950-5ab3feaf1894','PT Ari Ganteng', 'Jl. Merdeka No. 1, Jakarta', '/company-file/1726593321.png', now(), now());
+INSERT INTO companies (id,code_company,name, address, image_company, created_at, updated_at) VALUES
+('073cebf0-4c4a-5d4f-9950-5ab3feaf1894','PAT-3239','PT Ari Ganteng', 'Jl. Merdeka No. 1, Jakarta', '/company-file/1726593321.png', now(), now());
 
 -- migrate:down
 DROP TABLE companies;
